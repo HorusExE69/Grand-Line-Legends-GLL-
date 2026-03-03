315e512542bb416e4396a0c2f48edcb042a4d38e
HorusExE69 <sguellit@gmail.com>
2026-03-03 18:58:56 +0100

    Architecture+Classes Personnage, Player et Capacity

---
+++ src/model/Capacity.h
@@ -0,0 +1,20 @@
+#ifndef CAPACITY_H
+#define CAPACITY_H
+
+#include <string>
+
+using namespace std;
+
+class Capacity
+{
+	private:
+		int damage;
+		int heal;
+		int type;
+		int nbAffect;
+		int loadTime;
+
+	public:
+};
+
+#endif PERSONNAGE_H
---
+++ src/model/Player.h
@@ -0,0 +1,22 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+#include "Personnage.h"
+
+using namespace std;
+
+class Player
+{
+	private:
+		string pseudo;
+		string lvl;
+
+		int berries;
+
+		Character* ownCharacter;
+
+	public:
+};
+
+#endif PERSONNAGE_H
