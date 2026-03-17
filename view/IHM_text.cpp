bf77aa52642e710af1e8b1e1e3d9db3dc42bd955
Lisa <lisa.iris@etu.univ-lyon1.fr>
2026-03-17 11:43:12 +0100

    ajout du dossier view et du fichier IHM_text.cpp

---
+++ view/IHM_text.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "../src/controller/Battle.h"
+#include "../src/controller/Shop.h"
+
+using namespace std;
+
+int main()
+{
+    int choice = 0;
+    while (choice != 3)
+    {
+            cout << "Bienvenue dans Grand Line Legends" << endl;
+            cout << "Voulez-vous :" << endl;
+            cout << "1.Jouer" << endl;
+            cout << "2.Acheter" << endl;
+            cout << "3.Quitter" << endl;
+
+            cin >> choice;
+            switch (choice) 
+            {
+                case 1:
+                {
+                    cout << "Vous voici dans la partie combat" << endl;
+                    Battle battle;
+                    battle.start();
+                    break;
+                }
+                case 2:
+                {
+                    cout << "Vous voici dans le magasin" << endl;
+                    open();
+                    break;
+                }
+                case 3:
+                {
+                    cout << "Merci d'avoir joué." << endl;
+                    cout <<  "Fin du programme." << endl;
+                    break;
+                }
+                default:
+                {
+                    cout << "Choix invalide" << endl;
+                }
+            }
+    }
+
+    return 0;
+}
