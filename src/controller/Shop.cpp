df2cd4ff73aeb52e045137f6fbec8d93170e1312
Lisa <lisa.iris@etu.univ-lyon1.fr>
2026-03-17 12:46:42 +0100

    ajout shop.h

--- src/controller/Shop.cpp
+++ src/controller/Shop.cpp
@@ -3,7 +3,36 @@
 
 using namespace std;
 
-void open()
+Shop::Shop(int max_c)
 {
-    cout << "Bienvenue dans le magasin !" << endl;
+    for (int i=0, i < max_c; i++)
+    {
+        coffre.push_back(Coffre(i));
+    }
+}
+
+void Shop::open()
+{
+    cout << "Bienvenue dans le Shop Merry !" << endl;
+    cout << "Coffres disponibles :" << endl;
+    for (size_t i=0; i < coffre.size(); i++)
+    {
+        cout << i << ". Coffre ID " << coffre[i].getID() << endl;
+    }
+}
+
+void Shop::close()
+{
+    cout << "Fermeture du Shop Merry" << endl;
+}
+
+void Shop::openCoffre(int index)
+{
+    if (index < 0 || index >= )
+    {
+        cout << "Coffre invalide" << endl;
+        return;
+    }
+    cout << "Ouverture du coffre " << index << "..." <<endl;
+    coffre.open(index);
 }
--- src/controller/Shop.h
+++ src/controller/Shop.h
@@ -1,7 +1,19 @@
 #ifndef SHOP_H
 #define SHOP_H
 
-void open();
-void exit();
+#include "Coffre.h"
+
+class Shop 
+{
+    private:
+        Coffre* coffre;
+        int nb_max_c;
+    public:
+        Shop(int max_c = 60);
+        ~Shop();
+        void open();
+        void exit();
+        void openCoffre(int index);
+};
 
 #endif
